22027ebd6376e139e79223a5c7aec77b27180f3f
jcanett1 <jcanett@pxg.com>
2025-07-15 11:13:05 -0700

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,6 +1,12 @@
 
 import Swal from 'sweetalert2'
 
+// Inicializar Supabase aquí mismo
+const supabaseUrl = 'https://bwkvfwrrlizhqdpaxfmb.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3a3Zmd3JybGl6aHFkcGF4Zm1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NTIyODMsImV4cCI6MjA2NTMyODI4M30.6ryUGUVRcDtASw0s1RTnKwSA4ezn_I_oxHeuSWGmwFU';
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+
 document.addEventListener('DOMContentLoaded', async function() {
   // Cargar datos iniciales
   await cargarProductos()
